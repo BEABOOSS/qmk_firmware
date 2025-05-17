cfb3b527caacdecb801126cc132ac92e864067f8
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2025-05-17 11:22:34 -0400

    added f2 and f3 for valorant on GAME layer

--- keyboards/dztech/dz60rgb_ansi/keymaps/BEABOOSS/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/BEABOOSS/keymap.c
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
         KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS,
         KC_LSFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   , KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , KC_BSPC, KC_RSFT,
         KC_CAPS, KC_X   , KC_C   , KC_V   , KC_B   , KC_Z   , KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT ,
-        KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                                KC_LALT, KC_RCTL, DF(BASE), _______
+        KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                                KC_LALT, KC_F2, DF(BASE), KC_F3
     )
 };
 // clang-format on
