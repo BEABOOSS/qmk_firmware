fad505ceaf5b05518086eacb6320e105ec07b1d5
BEABOOSS <72757983+BEABOOSS@users.noreply.github.com>
2025-09-20 18:48:41 -0400

    almost

--- keyboards/dztech/dz60rgb_ansi/keymaps/miryoku_2/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/miryoku_2/keymap.c
@@ -6,10 +6,17 @@
 #ifdef SELECT_WORD_ENABLE
 #    include "features/select_word.h"
 #endif // SELECT_WORD_ENABLE
+#include "print.h"
+
+#ifndef MAGIC_ENABLE
+uint8_t mod_config(uint8_t mod) {
+    return mod;
+}
+#endif
 
 enum layers {
     BASE,
-    TAP,
+    // TAP,
     NAV,
     MOUSE,
     MEDIA,
@@ -20,7 +27,7 @@ enum layers {
 };
 enum {
     U_TD_BOOT,
-    U_TD_U_TAP,
+    // U_TD_U_TAP,
     U_TD_U_BASE,
     U_TD_U_NUM,
     U_TD_U_NAV,
@@ -47,11 +54,11 @@ void u_td_fn_U_BASE(tap_dance_state_t *state, void *user_data) {
 }
 
 // to be fixed
-void u_td_fn_U_TAP(tap_dance_state_t *state, void *user_data) {
-    if(state->count == 2) {
-        default_layer_set((layer_state_t)1 << GAME);
-    }
-}
+// void u_td_fn_U_TAP(tap_dance_state_t *state, void *user_data) {
+//     if(state->count == 2) {
+//         default_layer_set((layer_state_t)1 << GAME);
+//     }
+// }
 
 void u_td_fn_U_NAV(tap_dance_state_t *state, void *user_data) {
     if(state->count == 2) {
@@ -144,6 +151,52 @@ bool alt_mod_active = false;
 #define TN_BSP LT(NUM,KC_BSPC)
 #define TF_DEL LT(FUN,KC_DEL)
 
+KEYCODE_STRING_NAMES_USER(
+    KEYCODE_STRING_NAME(ALT_MOD),
+    // KEYCODE_STRING_NAME(RGBBRI),
+    // KEYCODE_STRING_NAME(RGBNEXT),
+    // KEYCODE_STRING_NAME(RGBHUP),
+    // KEYCODE_STRING_NAME(RGBHRND),
+    // KEYCODE_STRING_NAME(RGBDEF1),
+    // KEYCODE_STRING_NAME(RGBDEF2),
+    // KEYCODE_STRING_NAME(M_NOOP),
+#ifdef SELECT_WORD_ENABLE
+    KEYCODE_STRING_NAME(SELLINE),
+    KEYCODE_STRING_NAME(SELWBAK),
+    KEYCODE_STRING_NAME(SELWFWD),
+#endif // SELECT_WORD_ENABLE
+    // KEYCODE_STRING_NAME(U_TD_BOOT),
+    // KEYCODE_STRING_NAME(U_TD_U_TAP),
+    // KEYCODE_STRING_NAME(U_TD_U_BASE),
+    // KEYCODE_STRING_NAME(U_TD_U_NUM),
+    // KEYCODE_STRING_NAME(U_TD_U_NAV),
+    // KEYCODE_STRING_NAME(U_TD_U_MOUSE),
+    // KEYCODE_STRING_NAME(U_TD_U_MEDIA),
+    // KEYCODE_STRING_NAME(U_TD_U_SYM),
+    // KEYCODE_STRING_NAME(U_TD_U_FUN),
+    // KEYCODE_STRING_NAME(U_TD_U_GAME),
+    KEYCODE_STRING_NAME(HRM_A),
+    KEYCODE_STRING_NAME(HRM_R),
+    KEYCODE_STRING_NAME(HRM_S),
+    KEYCODE_STRING_NAME(HRM_T),
+    KEYCODE_STRING_NAME(HRM_N),
+    KEYCODE_STRING_NAME(HRM_E),
+    KEYCODE_STRING_NAME(HRM_I),
+    KEYCODE_STRING_NAME(HRM_O),
+    KEYCODE_STRING_NAME(TME_ESC),
+    KEYCODE_STRING_NAME(TN_SPC),
+    KEYCODE_STRING_NAME(TMO_TAB),
+    KEYCODE_STRING_NAME(TS_ENT),
+    KEYCODE_STRING_NAME(TN_BSP),
+    KEYCODE_STRING_NAME(TF_DEL)
+);
+
+static const char* const LAYER_NAMES[] = {
+    "BASE","NAV","MOUSE","MEDIA","NUM","SYM","FUN","GAME"
+};
+
+
+
 //
 // gui plus alt
 // shift and ctrl
@@ -172,7 +225,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ),
 
     [NAV] = LAYOUT_60_ansi( // Symbols red
-        TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_GAME),  TD(U_TD_U_BASE), U_NA, U_NP, U_NP, U_NP, U_RDO,   U_PST,   U_CPY,   U_CUT,  U_UND,    U_NP,
+        TD(U_TD_BOOT), TD(U_TD_U_NAV), TD(U_TD_U_GAME),  TD(U_TD_U_BASE), U_NA, U_NP, U_NP, U_NP, U_RDO,   U_PST,   U_CPY,   U_CUT,  U_UND,    U_NP,
         KC_LALT,       KC_LGUI,        KC_LSFT,          KC_LCTL,         U_NA, U_NP, U_NP, U_NP, CW_TOGG, KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT,  U_NP,
         U_NA,          KC_ALGR,        TD(U_TD_U_NUM),   TD(U_TD_U_NAV),  U_NA, U_NP, U_NP, U_NP, KC_INS,  KC_HOME, KC_PGDN, KC_PGUP,          KC_END,
         U_NP,          U_NA,           U_NA,             U_NA,            U_NP, U_NP, U_NP, U_NP, KC_ENT,  KC_BSPC, KC_DEL,                    U_NP,
@@ -180,23 +233,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ),
 
     [MOUSE] = LAYOUT_60_ansi(  // Navigation blue
-        TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_GAME),  TD(U_TD_U_BASE), U_NA,    U_NP, U_NP, U_NP, U_RDO,        U_PST,   U_CPY,   U_CUT,   U_UND,   U_NP,
-        KC_LALT,       KC_LGUI,        KC_LSFT,          KC_LCTL,         U_NA,    U_NP, U_NP, U_NP, U_NA,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, U_NP,
-        U_NA,          KC_ALGR,        TD(U_TD_U_SYM),   TD(U_TD_U_MOUSE),U_NA,    U_NP, U_NP, U_NP, U_NA,         KC_WH_L, KC_WH_D, KC_WH_U,          KC_WH_R,
-        U_NP,          U_NA,           U_NA,             U_NA,            U_NP,    U_NP, U_NP, U_NP, KC_BTN2,      KC_BTN1, TF_DEL ,                   U_NP,
+        TD(U_TD_BOOT), TD(U_TD_U_NAV), TD(U_TD_U_GAME),  TD(U_TD_U_BASE), U_NA,    U_NP, U_NP, U_NP, U_RDO,        U_PST,   U_CPY,   U_CUT,   U_UND,   U_NP,
+        KC_LALT,       KC_LGUI,        KC_LSFT,          KC_LCTL,         U_NA,    U_NP, U_NP, U_NP, U_NA,         MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, U_NP,
+        U_NA,          KC_ALGR,        TD(U_TD_U_SYM),   TD(U_TD_U_MOUSE),U_NA,    U_NP, U_NP, U_NP, U_NA,         MS_WHLL, MS_WHLD, MS_WHLU,          MS_WHLR,
+        U_NP,          U_NA,           U_NA,             U_NA,            U_NP,    U_NP, U_NP, U_NP, MS_BTN2,      MS_BTN1, MS_BTN3,                   U_NP,
         U_NP, U_NP,    U_NP,                                                       U_NP,                                    U_NP,    U_NP,       U_NP, U_NP
     ),
 
     [MEDIA] = LAYOUT_60_ansi(  // Number PURPLE
-        TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_GAME),  TD(U_TD_U_BASE), U_NA,    U_NP, U_NP, U_NP, RGB_TOG, RGB_MOD,   RGB_HUI, RGB_SAI, RGB_VAI, U_NP,
-        KC_LALT,       KC_LGUI,        KC_LSFT,          KC_LCTL,         U_NA,    U_NP, U_NP, U_NP, U_NU,    KC_MPRV,   KC_VOLD, KC_VOLU, KC_MNXT, U_NP,
-        DB_TOGG,          KC_ALGR,        TD(U_TD_U_FUN),   TD(U_TD_U_MEDIA),U_NA,    U_NP, U_NP, U_NP, OU_AUTO, U_NU,      U_NU,    U_NU,             U_NU,
-        U_NP,          U_NA,           U_NA,             U_NA,            U_NP,    U_NP, U_NP, U_NP, KC_MSTP, KC_MPLY,   KC_MUTE,                   U_NP,
+        TD(U_TD_BOOT), TD(U_TD_U_NAV), TD(U_TD_U_GAME),  TD(U_TD_U_BASE), U_NA,    U_NP, U_NP, U_NP, UG_TOGG, U_NU,    U_NU,    U_NU,    U_NU,    U_NP,
+        KC_LALT,       KC_LGUI,        KC_LSFT,          KC_LCTL,         U_NA,    U_NP, U_NP, U_NP, U_NU,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, U_NP,
+        DB_TOGG,       KC_ALGR,        TD(U_TD_U_FUN),   TD(U_TD_U_MEDIA),U_NA,    U_NP, U_NP, U_NP, OU_AUTO, U_NU,    U_NU,    U_NU,             U_NU,
+        U_NP,          U_NA,           U_NA,             U_NA,            U_NP,    U_NP, U_NP, U_NP, KC_MSTP, KC_MPLY, KC_MUTE,                   U_NP,
         U_NP, U_NP,    U_NP,                                                       U_NP,                               U_NP,    U_NP,       U_NP, U_NP
     ),
 
     [NUM] = LAYOUT_60_ansi(  // Window management layer GREEN
-        KC_LBRC,      KC_7,            KC_8,             KC_9,            KC_RBRC, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_BASE), TD(U_TD_U_GAME),  TD(U_TD_U_TAP), TD(U_TD_BOOT), U_NP,
+        KC_LBRC,      KC_7,            KC_8,             KC_9,            KC_RBRC, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_BASE), TD(U_TD_U_GAME),  TD(U_TD_U_NAV), TD(U_TD_BOOT), U_NP,
         KC_SCLN,      KC_4,            KC_5,             KC_6,            KC_EQL,  U_NP, U_NP, U_NP, U_NA, KC_LCTL,         KC_LSFT,          KC_LGUI,        KC_LALT,       U_NP,
         KC_GRV,       KC_1,            KC_2,             KC_3,            KC_BSLS, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_NUM),  TD(U_TD_U_NAV),   KC_ALGR,                       U_NA,
         U_NP,         KC_DOT,          KC_0,             KC_MINS,         U_NP,    U_NP, U_NP, U_NP, U_NA, U_NA,            U_NA,                                            U_NP,
@@ -204,7 +257,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ),
 
     [SYM] = LAYOUT_60_ansi(  // Funky fun layer white
-        KC_LCBR,      KC_AMPR,        KC_ASTR,           KC_LPRN,         KC_RCBR, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_BASE), TD(U_TD_U_GAME),  TD(U_TD_U_TAP), TD(U_TD_BOOT), U_NP,
+        KC_LCBR,      KC_AMPR,        KC_ASTR,           KC_LPRN,         KC_RCBR, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_BASE), TD(U_TD_U_GAME),  TD(U_TD_U_NAV), TD(U_TD_BOOT), U_NP,
         KC_COLN,      KC_DLR,         KC_PERC,           KC_CIRC,         KC_PLUS, U_NP, U_NP, U_NP, U_NA, KC_LCTL,         KC_LSFT,          KC_LGUI,        KC_LALT,       U_NP,
         KC_TILD,      KC_EXLM,        KC_AT,             KC_HASH,         KC_PIPE, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_NUM),  TD(U_TD_U_NAV),   KC_ALGR,                       U_NA,
         U_NP,         KC_LPRN,        KC_RPRN,           KC_UNDS,         U_NP,    U_NP, U_NP, U_NP, U_NA, U_NA,            U_NA,                                            U_NP,
@@ -212,7 +265,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ),
 
     [FUN] = LAYOUT_60_ansi(  // Mouse and extra white
-        KC_F12,       KC_F7,          KC_F8,             KC_F9,           KC_PSCR, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_BASE), TD(U_TD_U_GAME),  TD(U_TD_U_TAP), TD(U_TD_BOOT), U_NP,
+        KC_F12,       KC_F7,          KC_F8,             KC_F9,           KC_PSCR, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_BASE), TD(U_TD_U_GAME),  TD(U_TD_U_NAV), TD(U_TD_BOOT), U_NP,
         KC_F11,       KC_F4,          KC_F5,             KC_F6,           KC_SCRL, U_NP, U_NP, U_NP, U_NA, KC_LCTL,         KC_LSFT,          KC_LGUI,        KC_LALT,       U_NP,
         KC_F10,       KC_F1,          KC_F2,             KC_F3,           KC_PAUS, U_NP, U_NP, U_NP, U_NA, TD(U_TD_U_FUN),  TD(U_TD_U_MEDIA), KC_ALGR,                       U_NA,
         U_NP,         KC_APP,         KC_SPC,            KC_TAB,          U_NP,    U_NP, U_NP, U_NP, U_NA, U_NA,            U_NA,                                            U_NP,
@@ -220,11 +273,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ),
 
     [GAME] = LAYOUT_60_ansi(  // Gaming Layer
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
-        KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_LSFT, KC_A   , KC_R   , KC_S   , KC_T   , KC_D   , KC_H   , KC_N   , KC_E   , KC_I   , KC_O   , KC_BSPC, KC_RSFT,
-        KC_CAPS, KC_X   , KC_C   , KC_V   , KC_B   , KC_Z   , KC_K   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_ENT ,
-        KC_LCTL, KC_LGUI, KC_LALT,                         KC_SPC,                                KC_LALT, KC_F2, DF(BASE), KC_F3
+        KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, U_NP, U_NP, U_NP, KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, U_NP,
+        KC_A,    KC_R,    KC_S,    KC_T,    KC_D, U_NP, U_NP, U_NP, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    U_NP,
+        KC_X,    KC_C,    KC_V,    KC_B,    KC_Z, U_NP, U_NP, U_NP, KC_K,    KC_M,    KC_COMM, ALT_MOD,          KC_SLSH,
+        U_NP,    KC_ESC,  KC_SPC,  KC_TAB,  U_NP, U_NP, U_NP, U_NP, TS_ENT,  TN_BSP,  TF_DEL,                    U_NP,
+        U_NP, U_NP,       U_NP,                         U_NP,                         U_NP,    U_NP,    U_NP,    U_NP
     )
 };
 
@@ -250,8 +303,9 @@ uint8_t myrand(void) {
 
 const uint16_t PROGMEM thumbcombos_base_right[] = {LT(SYM, KC_ENT), LT(NUM, KC_BSPC), COMBO_END};
 const uint16_t PROGMEM thumbcombos_base_left[] = {LT(NAV, KC_SPC), LT(MOUSE, KC_TAB), COMBO_END};
+const uint16_t PROGMEM thumbcombos_base_G_r[] = {TN_BSP, TF_DEL, COMBO_END};
 const uint16_t PROGMEM thumbcombos_nav[] = {KC_ENT, KC_BSPC, COMBO_END};
-const uint16_t PROGMEM thumbcombos_mouse[] = {KC_BTN2, KC_BTN1, COMBO_END};
+const uint16_t PROGMEM thumbcombos_mouse[] = {MS_BTN2, MS_BTN1, COMBO_END};
 const uint16_t PROGMEM thumbcombos_media[] = {KC_MSTP, KC_MPLY, COMBO_END};
 const uint16_t PROGMEM thumbcombos_num[] = {KC_0, KC_MINS, COMBO_END};
 const uint16_t PROGMEM thumbcombos_sym[] = {KC_RPRN, KC_UNDS, COMBO_END};
@@ -259,8 +313,9 @@ const uint16_t PROGMEM thumbcombos_fun[] = {KC_SPC, KC_TAB, COMBO_END};
 combo_t key_combos[] = {
     COMBO(thumbcombos_base_right, LT(FUN, KC_DEL)),
     COMBO(thumbcombos_base_left, LT(MEDIA, KC_ESC)),
+    COMBO(thumbcombos_base_G_r, LT(NUM, KC_BSPC)),
     COMBO(thumbcombos_nav, KC_DEL),
-    COMBO(thumbcombos_mouse, KC_BTN3),
+    COMBO(thumbcombos_mouse, MS_BTN3),
     COMBO(thumbcombos_media, KC_MUTE),
     COMBO(thumbcombos_num, KC_DOT),
     COMBO(thumbcombos_sym, KC_LPRN),
@@ -428,7 +483,7 @@ void set_layer_color(uint8_t led_min, uint8_t led_max, uint8_t layer, uint8_t re
 }
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    uint8_t layer = get_highest_layer(layer_state);
+    uint8_t layer = get_highest_layer(layer_state | default_layer_state);
     switch (layer) {
         case NAV:
             set_layer_color(led_min, led_max, layer, RGB_BLUE);
@@ -448,6 +503,9 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         case FUN:
             set_layer_color(led_min, led_max, layer, RGB_RED);
             break;
+        case GAME:
+            set_layer_color(led_min, led_max, layer, RGB_OFF);
+            break;
         default:
             set_layer_color(led_min, led_max, layer, RGB_WHITE);
             break;
@@ -457,49 +515,65 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 }
 #endif // RGB_MATRIX_ENABLE
 
+// Log when the highest momentary layer changes.
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = get_highest_layer(state);
+    const char* name = (layer < (sizeof(LAYER_NAMES)/sizeof(LAYER_NAMES[0]))) ? LAYER_NAMES[layer] : "?";
+    dprintf("Layer changed: %u (%s)\n", layer, name);
+    return state;
+}
+
+// Log when the default (persistent) layer changes.
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    uint8_t layer = get_highest_layer(state);
+    const char* name = (layer < (sizeof(LAYER_NAMES)/sizeof(LAYER_NAMES[0]))) ? LAYER_NAMES[layer] : "?";
+    dprintf("Default layer changed: %u (%s)\n", layer, name);
+    return state;
+}
+
 // clang-format off
 
 ///////////////////////////////////////////////////////////////////////////////
 // Debug logging
 ///////////////////////////////////////////////////////////////////////////////
 
-#if !defined(NO_DEBUG) && defined(KEYCODE_STRING_ENABLE)
+// #if !defined(NO_DEBUG) && defined(KEYCODE_STRING_ENABLE)
 
-#pragma message "dlog_record: enabled"
-#include "print.h"
-// #include "features/keycode_string.h"
+// #pragma message "dlog_record: enabled"
+// #include "print.h"
+// // #include "features/keycode_string.h"
 
-// KEYCODE_STRING_NAMES_USER(
-//   KEYCODE_STRING_NAME(SELLINE),
-//   KEYCODE_STRING_NAME(SELWBAK),
-//   KEYCODE_STRING_NAME(SELWFWD),
-//   KEYCODE_STRING_NAME(RGBBRI),
-//   KEYCODE_STRING_NAME(RGBNEXT),
-//   KEYCODE_STRING_NAME(RGBHUP),
-//   KEYCODE_STRING_NAME(RGBHRND),
-//   KEYCODE_STRING_NAME(RGBDEF1),
-//   KEYCODE_STRING_NAME(RGBDEF2),
-// );
+// // KEYCODE_STRING_NAMES_USER(
+// //   KEYCODE_STRING_NAME(SELLINE),
+// //   KEYCODE_STRING_NAME(SELWBAK),
+// //   KEYCODE_STRING_NAME(SELWFWD),
+// //   KEYCODE_STRING_NAME(RGBBRI),
+// //   KEYCODE_STRING_NAME(RGBNEXT),
+// //   KEYCODE_STRING_NAME(RGBHUP),
+// //   KEYCODE_STRING_NAME(RGBHRND),
+// //   KEYCODE_STRING_NAME(RGBDEF1),
+// //   KEYCODE_STRING_NAME(RGBDEF2),
+// // );
 
-static void dlog_record(uint16_t keycode, keyrecord_t* record) {
-  if (!debug_enable) { return; }
-  uint8_t layer = read_source_layers_cache(record->event.key);
-  bool is_tap_hold = IS_QK_MOD_TAP(keycode) || IS_QK_LAYER_TAP(keycode);
-  xprintf("L%-2u ", layer);  // Log the layer.
-  if (IS_COMBOEVENT(record->event)) {  // Combos don't have a position.
-    xprintf("combo   ");
-  } else {  // Log the "(row,col)" position.
-    xprintf("(%2u,%2u) ", record->event.key.row, record->event.key.col);
-  }
-  xprintf("%-4s %-7s %s\n",  // "(tap|hold) (press|release) <keycode>".
-      is_tap_hold ? (record->tap.count ? "tap" : "hold") : "",
-      record->event.pressed ? "press" : "release",
-      get_keycode_string(keycode));
-}
-#else
-#pragma message "dlog_record: disabled"
-#define dlog_record(keycode, record)
-#endif  // !defined(NO_DEBUG) && defined(KEYCODE_STRING_ENABLE)
+// static void dlog_record(uint16_t keycode, keyrecord_t* record) {
+//   if (!debug_enable) { return; }
+//   uint8_t layer = read_source_layers_cache(record->event.key);
+//   bool is_tap_hold = IS_QK_MOD_TAP(keycode) || IS_QK_LAYER_TAP(keycode);
+//   xprintf("L%-2u ", layer);  // Log the layer.
+//   if (IS_COMBOEVENT(record->event)) {  // Combos don't have a position.
+//     xprintf("combo   ");
+//   } else {  // Log the "(row,col)" position.
+//     xprintf("(%2u,%2u) ", record->event.key.row, record->event.key.col);
+//   }
+//   xprintf("%-4s %-7s %s\n",  // "(tap|hold) (press|release) <keycode>".
+//       is_tap_hold ? (record->tap.count ? "tap" : "hold") : "",
+//       record->event.pressed ? "press" : "release",
+//       get_keycode_string(keycode));
+// }
+// #else
+// #pragma message "dlog_record: disabled"
+// #define dlog_record(keycode, record)
+// #endif  // !defined(NO_DEBUG) && defined(KEYCODE_STRING_ENABLE)
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -525,9 +599,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         return false;
     }
 #endif // SELECT_WORD_ENABLE
-
-    dlog_record(keycode, record);
-    //const uint8_t layer = read_source_layers_cache(record->event.key);
+    uint8_t layer = get_highest_layer(layer_state | default_layer_state);
+    const char* name = (layer < (sizeof(LAYER_NAMES)/sizeof(LAYER_NAMES[0]))) ? LAYER_NAMES[layer] : "?";
+    dprintf("Layer: %u (%s)\n", layer, name);
+    dprintf("kc: %s\n", get_keycode_string(keycode));
+    //dlog_record(keycode, record);
+    // const uint8_t layer = read_source_layers_cache(record->event.key);
 
     switch (keycode) {
         case ALT_MOD:
